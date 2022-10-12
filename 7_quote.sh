1930a13c0de67020e854628c944d5492e841deee
priyabrata <priyabratapinku7@gmail.com>
2022-10-12 15:46:22 +0530

    bash-scripting

---
+++ 7_quote.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VIRUS='covid19'
+
+echo "Due to $VIRUS virus, company lost $9 million"
+#Due to covid19 virus, company lost   million
+
+echo 'Due to $VIRUS virus, company lost $9 million'
+#Due to $VIRUS virus, company lost $9 million
+
+echo "Due to $VIRUS virus, company lost \$9 million"
+#Due to covid19 virus, company lost $9 million
