547d0ed80aa5cf07588a3dd43a07be70202c7bac
priyabrata <priyabratapinku7@gmail.com>
2022-10-12 18:40:52 +0530

    bash-scripting

---
+++ 9_testvars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "The $SEASON season is more than expected, this time"
+
+#SEASON="Monsoon"
+
+#export SEASON
+
+#It will pass $SEASON value to child shell
+
+#./9_testvars.sh
+
+#The Monsoon season is more than expected, this time
